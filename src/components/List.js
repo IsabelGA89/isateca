fc8d7f0a0d06c021d8d985c83d3a9721bd57eeb2
IsabelGA89 <isabelgonzalezanzano@gmail.com>
2021-07-04 19:01:33 +0200

    Terminada estructura básica
    Pendiente gestión de imágenes

---
+++ src/components/List.js
@@ -0,0 +1,21 @@
+import Item from "./Item";
+
+function List(props) {
+  return (
+    <div className="list">
+      {props.items.map((item) => (
+        <Item
+          key={item.id}
+          id={item.id}
+          title={item.title}
+          image={item.image}
+          rating={item.rating}
+          onremove={props.onremove}
+          onupdaterating={props.onupdaterating}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default List;
